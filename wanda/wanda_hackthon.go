ff681af72c993fd5b9499afd0dce4b91705509b9
YeyingLiu <liuyeying1103@163.com>
2017-03-11 20:23:42 +0800

    571

--- wanda/wanda_hackthon.go
+++ wanda/wanda_hackthon.go
@@ -568,7 +568,7 @@ func GetRegulation(stub shim.ChaincodeStubInterface, regulationId string)(Regula
 	var regulation Regulation
 	
 	regulationBytes, err := stub.GetState("regulation" + strconv.Itoa(0))
-	if regulationId == nil {
+	if regulationId == null {
 		fmt.Println("Error unmarshalling cp " + regulationId)
 		return regulation, errors.New("Error unmarshalling cp " + regulationId)
 	}
